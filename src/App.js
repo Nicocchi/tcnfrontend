c950a4888ced3d7ff2fce13fcf5c4e36e8861b95
Nicocchi <nicocchilover@gmail.com>
2020-07-03 02:38:39 -0400

    nav and header implemented design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import TopBar from './Components/Navigation/TopBar/TopBar';
+import Header from './Components/Header/Header';
+import NavBar from './Components/Navigation/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopBar />
+      <Header />
+      <NavBar />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import rootReducer from "./Store/reducers";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let store = null;
+
+if (typeof window !== "undefined") {
+    const composeEnhancers =
+        typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+            ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+                  // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+              })
+            : compose;
+
+    const devEnhancer = composeEnhancers(applyMiddleware(thunk, logger));
+    const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+    // Create redux store
+    if (process.env.REACT_APP_NODE_ENV !== "production") {
+        store = createStore(rootReducer, devEnhancer);
+    } else {
+        store = createStore(rootReducer, enhancer);
+    }
+
+    ReactDOM.render(
+      <React.StrictMode>
+          <Provider store={store}>
+              <Router>
+                  <App />
+              </Router>
+          </Provider>
+      </React.StrictMode>,
+      document.getElementById("root")
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
